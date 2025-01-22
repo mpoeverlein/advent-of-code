ab983543f3ea9f1fdccae92d634d9f1311499bcf
mpoeverlein <max.poeverlein@gmail.com>
2025-01-22 19:21:58 +0100

    add docstrings for day 14

--- 2024/a-14-01.py
+++ 2024/a-14-01.py
@@ -1,16 +1,50 @@
 class Robot:
     def __init__(self, px, py, vx, vy) -> None:
+        '''
+        Create Robot object with position and velocity.
+
+        Parameters
+        ----------
+        px, py: int, int
+          Position of Robot
+        vx, vy: int, int
+          Velocity of Robot, i.e. how many steps moving in 1 second
+        '''
         self.p = [px, py]
         self.v = [vx, vy]
-    def propagate(self, time_steps, dim_x, dim_y) -> None:
-        self.p = [(self.p[0] + time_steps * self.v[0]) % dim_x,
-                  (self.p[1] + time_steps * self.v[1]) % dim_y
-        ]
 
     def __repr__(self):
         return f'Robot at ({self.p[0]},{self.p[1]}) with v=({self.v[0]},{self.v[1]})'
 
-    def quadrant(self, dim_x, dim_y):
+    def propagate(self, time_steps: int, dim_x: int, dim_y: int) -> None:
+        '''
+        Propagate Robot position for <time_steps> seconds, accounting for boundary conditions due to dim_x, dim_y.
+
+        Parameters
+        ----------
+        time_steps: int
+          Number of seconds for which to propagate
+        dim_x, dim_y: int, int
+          Number of tiles in x/y direction
+        '''
+        self.p = [(self.p[0] + time_steps * self.v[0]) % dim_x,
+                  (self.p[1] + time_steps * self.v[1]) % dim_y
+        ]
+
+    def quadrant(self, dim_x: int, dim_y: int):
+        '''
+        Determine in which quadrant Robot is located.
+        Return -1 if Robot is exactly on border between two quadrants.
+
+        Parameters
+        ----------
+        dim_x, dim_y: int, int
+          Number of tiles in x/y direction
+
+        Returns
+        -------
+        n_quadrant: int, 0, 1, 2 or 3
+        '''
         px,py = self.p
         if px == dim_x // 2:
             return -1
@@ -20,6 +54,17 @@ class Robot:
         return 2 * int(py > dim_y // 2) + int(px > dim_x // 2)
 
 def read_data(input_filename: str) -> list[Robot]:
+    '''
+    Read info from input file and create robots.
+
+    Parameters
+    ----------
+    input_filename: str
+
+    Returns
+    -------
+    robots: list[Robot]
+    '''
     with open(input_filename, 'r') as f:
         lines = f.readlines()
 
@@ -33,28 +78,46 @@ def read_data(input_filename: str) -> list[Robot]:
 
     return robots
 
-def product(v):
+def product(v: list[int]) -> int:
+    '''
+    Calculate product of all items in v.
+
+    Returns
+    -------
+    prod: int
+    '''
     prod = 1
     for vv in v:
         prod *= vv
     return prod
 
-if __name__ == '__main__':
-    # input_filename = 'z-14-02-actual-example.txt'
-    input_filename = 'z-14-01-input.txt'
-    # time_steps, dim_x, dim_y = 100, 11, 7
-    time_steps, dim_x, dim_y = 100, 101, 103
-    robots = read_data(input_filename)
-    print(robots)
+def determine_quadrant_count(robots: list[Robot], time_steps: int, dim_x: int, dim_y: int) -> int:
+    '''
+    Determine in which quadrant each robot is after <time_steps> seconds and calculate the product of the values.
+
+    Parameters
+    ----------
+    robots: list[Robot]
+    time_steps: int
+    dim_x, dim_y: int, int
+
+    Returns
+    product_quadrant_count: int
+    '''
     quadrant_count = {k: 0 for k in range(4)}
     for robot in robots:
         robot.propagate(time_steps, dim_x, dim_y)
-        print(robot)
         quad = robot.quadrant(dim_x, dim_y)
         if quad != -1:
             quadrant_count[quad] += 1
 
-    print(quadrant_count)
-    print(product(quadrant_count.values()))
+    return product(quadrant_count.values())
 
-# 83445876 is too low
+if __name__ == '__main__':
+    # input_filename = 'z-14-02-actual-example.txt'
+    input_filename = 'z-14-01-input.txt'
+    time_steps, dim_x, dim_y = 100, 101, 103
+    robots = read_data(input_filename)
+    quadrant_count = determine_quadrant_count(robots, time_steps, dim_x, dim_y)
+    print(f'The quadrant count is {quadrant_count}.')
+
--- 2024/a-14-02.py
+++ 2024/a-14-02.py
@@ -4,46 +4,9 @@ this script prints the robot configuration at every time step and we will
 visually find the answer
 '''
 
-class Robot:
-    def __init__(self, px, py, vx, vy) -> None:
-        self.p = [px, py]
-        self.v = [vx, vy]
-    def propagate(self, time_steps, dim_x, dim_y) -> None:
-        self.p = [(self.p[0] + time_steps * self.v[0]) % dim_x,
-                  (self.p[1] + time_steps * self.v[1]) % dim_y
-        ]
-
-    def __repr__(self):
-        return f'Robot at ({self.p[0]},{self.p[1]}) with v=({self.v[0]},{self.v[1]})'
-
-    def quadrant(self, dim_x, dim_y):
-        px,py = self.p
-        if px == dim_x // 2:
-            return -1
-        if py == dim_y // 2:
-            return -1
-
-        return 2 * int(py > dim_y // 2) + int(px > dim_x // 2)
-
-def read_data(input_filename: str) -> list[Robot]:
-    with open(input_filename, 'r') as f:
-        lines = f.readlines()
-
-    robots = []
-    for line in lines:
-        px = int(line.split('=')[1].split(',')[0])
-        py = int(line.split(',')[1].split()[0])
-        vx = int(line.split('=')[2].split(',')[0])
-        vy = int(line.split(',')[2].split()[0])
-        robots.append(Robot(px,py,vx,vy))
-
-    return robots
-
-def product(v):
-    prod = 1
-    for vv in v:
-        prod *= vv
-    return prod
+import importlib
+day_14_01 = importlib.import_module('a-14-01')
+Robot = day_14_01.Robot
 
 def make_field_string(robots: list[Robot], dim_x: int, dim_y: int) -> str:
     empty = [[' ' for i in range(dim_x)] for y in range(dim_y)]
@@ -55,18 +18,17 @@ def make_field_string(robots: list[Robot], dim_x: int, dim_y: int) -> str:
 if __name__ == '__main__':
     # input_filename = 'z-14-02-actual-example.txt'
     input_filename = 'z-14-01-input.txt'
-    robots = read_data(input_filename)
+    robots = day_14_01.read_data(input_filename)
     time_steps, dim_x, dim_y = 100, 101, 103
     # find christmas tree by visual inspection
-    for i in range(1,18_251):
+    for i in range(1,18_251): # 18,251 is where I found the pattern by visual inspection
         for robot in robots:
             robot.propagate(1, dim_x, dim_y)
-        print(dim_x*'*')
         print(i)
         field_string = make_field_string(robots, dim_x, dim_y)
         if 'OOOOOOOOO' in field_string:
             print(field_string)
+            break
 
-    # possible: 18250 is too high
-
+    print(f'The christmas tree first appears after {i} steps.')
 

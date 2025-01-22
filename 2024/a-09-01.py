565b32a24179009d5d9acdb200f00cb4c912e454
mpoeverlein <max.poeverlein@gmail.com>
2025-01-22 15:36:53 +0100

    add docstrings for day 9

--- 2024/a-09-01.py
+++ 2024/a-09-01.py
@@ -1,11 +1,38 @@
 from copy import deepcopy
 
-def read_data(f: str) -> list[int]:
-    with open(f, 'r') as ff:
-        line = ff.readline()
+def read_data(input_filename: str) -> list[int]:
+    '''
+    Read data from input filename and return as list of integers.
+
+    Parameters
+    ----------
+    input_filename: str
+
+    Returns
+    -------
+    result: list[int]
+    '''
+    with open(input_filename, 'r') as f:
+        line = f.readline()
     return [int(s) for s in line.strip()]
 
 def uncompress(data_list: list[int]) -> list[dict[str, int], dict[int, str]]:
+    '''
+    Translate input sequence into data dictionary and inverse data dictionary
+
+    Parameters
+    ----------
+    data_list: list[int]
+      Numbers, alternatingly indicating occupied and empty blocks
+
+    Returns
+    -------
+    data_dict: dict[str, int]
+      keys: blockID, values: location
+    inverse_data_dict: dict[int, str]
+      keys: location, values: blockID or empty
+    '''
+
     data_dict, inverse_data_dict = {}, {}
     pointer = 0
     id_counter = 0
@@ -25,10 +52,22 @@ def uncompress(data_list: list[int]) -> list[dict[str, int], dict[int, str]]:
     return data_dict, inverse_data_dict
 
 def print_data_dict(data_dict: dict[str, int]) -> str:
+    '''
+    Create string representing data of data_dict
+
+    Parameters
+    ----------
+    data_dict: dict[str, int]
+      keys: blockID, values: location
+
+    Returns
+    -------
+    result: str
+    '''
     max_length = 0
     for k,v in data_dict.items():
         max_length = max(max_length, max(v))
-    
+
     result = (max_length+1) * ['.']
     for k,v in data_dict.items():
         for j in v:
@@ -37,6 +76,18 @@ def print_data_dict(data_dict: dict[str, int]) -> str:
     return ''.join(result)
 
 def print_inverse_data_dict(inverse_data_dict: dict[str, int]) -> str:
+    '''
+    Create string representing data of inverse_data_dict
+
+    Parameters
+    ----------
+    inverse_data_dict: dict[int, str]
+      keys: location, values: blockID or empty
+
+    Returns
+    -------
+    result: str
+    '''
     max_length = max(inverse_data_dict.keys()) + 1
     result = max_length * ['.']
     for i in range(max_length):
@@ -44,7 +95,19 @@ def print_inverse_data_dict(inverse_data_dict: dict[str, int]) -> str:
 
     return ''.join(result)
 
-def move_blocks(inverse_data_dict):
+def move_blocks(inverse_data_dict: dict[str, int]) -> dict[str, int]:
+    '''
+    Move blocks such that every blocks was tried to be moved forward if empty blocks allow for it.
+
+    Parameters
+    ----------
+    inverse_data_dict: dict[int, str]
+      keys: location, values: blockID or empty
+
+    Returns
+    -------
+    reordered_data_dict: dict[str, int]
+    '''
     reordered_data_dict = deepcopy(inverse_data_dict)
     while True:
         last_full_block = find_right_block(reordered_data_dict)
@@ -60,18 +123,51 @@ def move_blocks(inverse_data_dict):
     return reordered_data_dict
 
 def find_right_block(d):
+    '''
+    Get location of last non-empty block.
+    Parameters
+    ----------
+    d: dict[int,str]
+
+    Returns
+    -------
+    k: int
+    '''
     for k,v in reversed(d.items()):
         if v != '.':
             return k
 
-def find_empty_block(d):
+def find_empty_block(d: dict[int,str]) -> int:
+    '''
+    Get location of first empty block.
+
+    Parameters
+    ----------
+    d: dict[int,str]
+
+    Returns
+    -------
+    k: int
+    '''
     for k,v in d.items():
         if v == '.':
             return k
 
-def filesystem_checksum(reordered_data_dict) -> int:
+def filesystem_checksum(reordered_data_dict: dict[int,str]) -> int:
+    '''
+    Calculate checksum from reordered data.
+
+    Parameters
+    ----------
+    reordered_data_dict: dict[int,str]
+
+    Returns
+    -------
+    counter: int
+      Checksum value
+    '''
+
     counter = 0
-    print(reordered_data_dict)
     for k,v in reordered_data_dict.items():
         if v == '.':
             return counter
@@ -79,18 +175,13 @@ def filesystem_checksum(reordered_data_dict) -> int:
 
     return counter
 
-input_filename = 'z-09-02-actual-example.txt'
-input_filename = 'z-09-01-input.txt'
-data_list = read_data(input_filename)
-# print(data_list)
-data_dict, inverse_data_dict = uncompress(data_list)
-# print(data_dict)
-# print(inverse_data_dict)
-result = print_data_dict(data_dict)
-# print(result)
-result = print_inverse_data_dict(inverse_data_dict)
-# print(result)
-reordered_data_dict = move_blocks(inverse_data_dict)
-# print(reordered_data_dict)
-# print(print_inverse_data_dict(reordered_data_dict))
-print(filesystem_checksum(reordered_data_dict))
+if __name__ == '__main__':
+    input_filename = 'z-09-02-actual-example.txt'
+    input_filename = 'z-09-01-input.txt'
+    data_list = read_data(input_filename)
+    data_dict, inverse_data_dict = uncompress(data_list)
+    result = print_data_dict(data_dict)
+    result = print_inverse_data_dict(inverse_data_dict)
+    reordered_data_dict = move_blocks(inverse_data_dict)
+    checksum = filesystem_checksum(reordered_data_dict)
+    print(f'The checksum is {checksum}.')
--- 2024/a-09-02.py
+++ 2024/a-09-02.py
@@ -1,11 +1,24 @@
 from copy import deepcopy
 '''
-represent both filled and empty blocks by DataBlock objects
-empty blocks have negative id_values, starting at the left with -1
+Trying the same approach as in a-09-01.py became quite cumbersome, so I opted for an object-oriented approach.
+Represent both filled and empty blocks by DataBlock objects.
+Empty blocks have negative id_values, starting at the left with -1
 '''
 
 class DataBlock:
     def __init__(self, id_value: int, start: int, length: int) -> None:
+        '''
+        Create DataBlock object.
+
+        Parameters
+        ----------
+        id_value: int
+          BlockID
+        start: int
+          location at which block starts
+        length: int
+          length of block
+        '''
         self.id_value = id_value
         self.start = start
         self.length = length
@@ -14,6 +27,14 @@ class DataBlock:
 
 class Disk:
     def __init__(self, input_filename: str) -> None:
+        '''
+        Create Disk object.
+        Disk.datablocks is created. It is a dictionary with blockIDs as keys and the corresponding DataBlock objects as values.
+
+        Parameters
+        ----------
+        input_filename: str
+        '''
         with open(input_filename, 'r') as ff:
             line = ff.readline()
         datablocks = []
@@ -30,12 +51,26 @@ class Disk:
                 position += value
                 empty -= 1
 
-        # self.datablocks = datablocks
         self.datablocks = {db.id_value: db for db in datablocks}
 
+    def __repr__(self) -> str:
+        self.remove_zero_length_datablocks()
+        result = ''
+        for db in self.get_sorted_datablocks():
+            if db.id_value < 0:
+                result += db.length * '.'
+            else:
+                result += db.length * str(db.id_value)
+        return result
+
     def clean_up(self) -> bool:
-        ''' remove zero length datablocks and merge adjacent empty blocks
-        to indicate how long to clean up, a boolean is returned (true if two empty fields were merged)
+        '''
+        Remove zero length datablocks and merge adjacent empty blocks
+        To indicate how long to clean up, a boolean is returned (true if two empty fields were merged)
+
+        Returns
+        -------
+        two_empty_fields_merged: bool
         '''
         self.remove_zero_length_datablocks()
         self.datablocks = self.get_sorted_datablocks()
@@ -49,25 +84,22 @@ class Disk:
                 return True
         return False
 
-
     def remove_zero_length_datablocks(self) -> None:
         self.datablocks = {k: v for k,v in self.datablocks.items() if v.length > 0}
 
     def get_sorted_datablocks(self) -> dict[int, DataBlock]:
+        '''
+        Sort Disk.datablocks by starting position of each DataBlock and return sorted dictionary
+
+        Returns
+        -------
+        datablocks: dict[int, DataBlock]
+          dictionary with blockIDs as keys and the corresponding DataBlock objects as values.
+        '''
         datablocks = [db for k,db in self.datablocks.items()]
         datablocks.sort(key=lambda x: x.start)
         self.datablocks = {db.id_value: db for db in datablocks}
         return self.datablocks
-
-    def __repr__(self) -> str:
-        self.remove_zero_length_datablocks()
-        result = ''
-        for db in self.get_sorted_datablocks():
-            if db.id_value < 0:
-                result += db.length * '.'
-            else:
-                result += db.length * str(db.id_value)
-        return result
 
     def get_start(self, id_value: int) -> int:
         return self.datablocks[id_value].start
@@ -82,10 +114,19 @@ class Disk:
         self.datablocks[id_value].length = new_length
 
     def move_block(self, move_id_value: int) -> bool:
-        ''' returns boolean: True: the block was moved, False: the block was not moved '''
-        print(move_id_value)
+        '''
+        Move block and returns boolean: True: the block was moved, False: the block was not moved
+
+        Parameters
+        ----------
+        move_id_value: int
+          blockID of block which is to be moved
+
+        Returns
+        -------
+        block_was_moved: bool
+        '''
         assert move_id_value >= 0, 'Only filled blocks movable! (ID >= 0)'
-        # self.remove_zero_length_datablocks()
         move_length = self.get_length(move_id_value)
         move_start = self.get_start(move_id_value)
         id_values = list(self.datablocks.keys())
@@ -97,7 +138,6 @@ class Disk:
                    and target_start < move_start):
                 continue
 
-            # self.datablocks.append(DataBlock(min(id_values)-1, move_start, move_length))
             self.datablocks.update({min(id_values)-1: DataBlock(min(id_values)-1, move_start, move_length)})
             self.set_start(move_id_value, target_start)
             self.set_start(target, target_start + move_length)
@@ -107,6 +147,9 @@ class Disk:
         return False
 
     def compress(self) -> None:
+        '''
+        This method compresses the data as much as possible.
+        '''
         id_values = list(self.datablocks.keys())
         for i in range(max(id_values), -1, -1):
             block_was_moved = self.move_block(i)
@@ -115,6 +158,14 @@ class Disk:
                     pass
 
     def checksum(self) -> int:
+        '''
+        Calculate checksum.
+
+        Returns
+        -------
+        counter: int
+          Checksum value
+        '''
         counter = 0
         for db in self.datablocks.values():
             if db.id_value < 0: continue
@@ -128,11 +179,7 @@ if __name__ == '__main__':
     # input_filename = 'z-09-02-actual-example.txt'
     input_filename = 'z-09-01-input.txt'
     disk = Disk(input_filename)
-    print(disk.datablocks)
-    # print(disk)
-    # disk.move_block(9)
     disk.compress()
-    # print(disk)
-    print(disk.datablocks)
-    print(disk.checksum())
+    checksum = disk.checksum()
+    print(f'The checksum is {checksum}.')
 

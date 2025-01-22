65bfd5c7a019e9fb6c3fcb3b1152e1e30dd3f909
mpoeverlein <max.poeverlein@gmail.com>
2025-01-22 15:59:17 +0100

    add docstrings for day 11

--- 2024/a-11-01.py
+++ 2024/a-11-01.py
@@ -1,9 +1,41 @@
+'''
+Although the number of items grows exponentially in the worst case, the following simple approach works for 25 repetitions.
+'''
+
 def read_data(input_filename: str) -> list[int]:
+    '''
+    Read data from input file.
+
+    Parameters
+    ----------
+    input_filename: str
+      Name of the input file from which to read data
+
+    Returns
+    -------
+    numbers_list: list[int]
+    '''
     with open(input_filename, 'r') as f:
         line = f.readline()
     return [int(s) for s in line.split()]
 
 def blink(stones: list[int]) -> list[int]:
+    '''
+    Apply blink rules:
+      1. if stone value is 0, make stone value 1
+      2. if number of digits in stone value is even, add two stones: one with first part of original stone value, the other with the second part
+      3. otherwise, multiply stone value by 2024
+
+    Parameters
+    ----------
+    stones: list[int]
+
+    Returns
+    -------
+    new_stones: list[int]
+      Stones list after blinking
+    '''
+
     new_stones = []
     for stone in stones:
         N = len(str(stone))
@@ -22,8 +54,7 @@ if __name__ == '__main__':
     input_filename = 'z-11-02-actual-example.txt'
     input_filename = 'z-11-01-input.txt'
     stones = read_data(input_filename)
-    print(stones)
     for _ in range(25):
         stones = blink(stones)
 
-    print(len(stones))
+    print(f'After 25 blinks, there are {len(stones)} stones.')
--- 2024/a-11-02.py
+++ 2024/a-11-02.py
@@ -12,25 +12,62 @@ This approach works because of the cyclical structures in the data, e.g.:
 
 class Stones:
     def __init__(self, data: dict[int, int]) -> None:
+        '''
+        Create Stones object with Stones.data stored.
+
+        Parameters
+        ----------
+        data: dict[int,int]
+          keys: all unique values of stones
+          values: frequency of each unique value
+        '''
         self.data = data
+
     def __repr__(self) -> str:
         return f'Stones object with stones: {self.data}'
+
     def __len__(self) -> int:
         return len(self.data)
 
     def add(self, stone: int, value: int) -> None:
+        '''
+        Add <value> number of stones to stone id with value <stone>.
+        Parameters
+        ----------
+        stone: int
+        value: int
+        '''
+
         if stone in self.data:
             self.data[stone] += value
         else:
             self.data[stone] = value
 
     def sum(self) -> int:
+        '''
+        Total sum of all numbers of stones.
+
+        Returns
+        -------
+        total: int
+        '''
         total = 0
         for stone, value in self.data.items():
             total += value
         return total
 
 def read_data(input_filename: str) -> Stones:
+    '''
+    Read data from input filename and create Stones object.
+
+    Parameter
+    ---------
+    input_filename: str
+
+    Returns
+    -------
+    stones: Stones
+    '''
     with open(input_filename, 'r') as f:
         line = f.readline()
     stones = Stones(data={0: 0}) # initialize empty Stones object
@@ -40,6 +77,23 @@ def read_data(input_filename: str) -> Stones:
     return stones
 
 def blink(stones: Stones) -> Stones:
+    '''
+    Apply blink rules:
+      1. if stone value is 0, make stone value 1
+      2. if number of digits in stone value is even, add two stones: one with first part of original stone value, the other with the second part
+      3. otherwise, multiply stone value by 2024
+
+    Due to the Stones object, the rules only have to be applied once for each possible stone value.
+
+    Parameters
+    ----------
+    stones: Stones
+
+    Returns
+    -------
+    new_stones: Stones
+      Stones after blinking
+    '''
     new_stones = Stones(data={0: 0})
     for stone, value in stones.data.items():
         N = len(str(stone))
@@ -59,11 +113,9 @@ if __name__ == '__main__':
     # input_filename = 'z-11-02-actual-example.txt'
     input_filename = 'z-11-01-input.txt'
     stones = read_data(input_filename)
-    print(stones)
     len_list = []
     for i in range(75): # blink 75 times
         stones = blink(stones)
-        # print(i, len(stones), stones)
         len_list.append(len(stones))
 
     print(f'Number of different stones: {len(stones)}')

bb55813cc62c043172894815c3d48ff0c134374d
mpoeverlein <max.poeverlein@gmail.com>
2025-01-29 17:24:06 +0100

    clean and add docstrings for day 18

--- 2024/a-18-01.py
+++ 2024/a-18-01.py
@@ -4,6 +4,17 @@ PositionData = tuple[int]
 from helpers import Field, Position
 
 def read_data(input_filename: str) -> list[Position]:
+    '''
+    Read in from input_filename, where bytes will be pushed down.
+
+    Parameters
+    ----------
+    input_filename: str
+
+    Returns
+    -------
+    pos: list[Position]
+    '''
     with open(input_filename, 'r') as f:
         lines = f.readlines()
     pos = []
@@ -13,13 +24,33 @@ def read_data(input_filename: str) -> list[Position]:
     return pos
 
 class RamField(Field):
+    '''
+    Object to store where bytes were pushed down and to find shortest path.
+    '''
     def add_bytes(self, positions: list[Position]) -> None:
+        '''
+        Change value in self.field at (y,x) to # for all items in positions.
+
+        Parameters
+        ----------
+        positions: list[Position]
+        '''
         for p in positions:
             self.set_value(p, '#')
 
-    # def __repr__(self) -> str:
-    #     result = ''
     def get_neighbors(self, position: Position) -> list[Position]:
+        '''
+        Get neighbors but only if they are not #.
+
+        Parameters
+        ----------
+        position: Position
+
+        Returns
+        -------
+        validated: list[Position]
+        '''
+
         y,x = position.y, position.x
         y_list, x_list = [y-1,y,y+1,y], [x,x+1,x,x-1]
         neighbors = [Position(i,j) for i,j in zip(y_list,x_list)]
@@ -27,45 +58,37 @@ class RamField(Field):
         validated = [n for n in validated if self.get_value(n) != '#']
         return validated
 
-    def find_path(self) -> None:
+    def find_shortest_path(self) -> int:
+        '''
+        Find path by computing the minimum path length between the start and every tile.
+
+        Returns
+        -------
+        shortest_path_length: int
+        '''
+
         self.position = Position(0,0)
         self.target = Position(self.Ny-1, self.Nx-1)
         path_length = {(0,0): 0}
         tiles_to_check = {(0,0): self.get_neighbors(Position(0,0))}
-        print(tiles_to_check)
         while len(tiles_to_check) > 0:
             previous = list(tiles_to_check.keys())[0]
             tile = tiles_to_check[previous].pop()
             if tile.position() not in path_length:
                 path_length[tile.position()] = path_length[previous] + 1
                 tiles_to_check.update({tile.position(): self.get_neighbors(tile)})
-
-            # print(tiles_to_check)
             tiles_to_check = {k: v for k,v in tiles_to_check.items() if len(v) > 0}
-            print(tiles_to_check)
 
-        print(path_length[self.target.position()])
-    
+        return path_length[self.target.position()]
 
 
 if __name__ == '__main__':
     input_filename = 'z-18-01-input.txt'
     # input_filename = 'z-18-02-actual-example.txt'
-    empty_input = 'z-18-03-empty-field-large.txt'
+    empty_input = 'z-18-03-empty-field-large.txt' # empty input needed to initialize RamField object
     # empty_input = 'z-18-04-empty-field-small.txt'
     pos = read_data(input_filename)
     field = RamField(empty_input, datatype=str)
-    print(field)
-    print(field.Nx, field.Ny)
-    # exit()
-    print(field.data_list)
     field.add_bytes(pos[:1024])
-
-    # for p in pos[:12]:
-    #     print(field.data_list[p.y])
-    #     # field.data_list[p.y][p.x] = '#'
-    #     field.set_value(p, '#')
-
-    print(field.find_path())
-    # 139 is too low
-    # 140 is too low
+    shortest_path = field.find_shortest_path()
+    print(f'The shortest path has {shortest_path} tiles.')
--- 2024/a-18-02.py
+++ 2024/a-18-02.py
@@ -2,33 +2,22 @@ FieldData = list[list]
 PositionData = tuple[int]
 
 from helpers import Field, Position
+import importlib
+day_18_01 = importlib.import_module('a-18-01')
 
-def read_data(input_filename: str) -> list[Position]:
-    with open(input_filename, 'r') as f:
-        lines = f.readlines()
-    pos = []
-    for line in lines:
-        x,y = line.split(',')
-        pos.append(Position(int(y),int(x)))
-    return pos
+class RamField(day_18_01.RamField):
+    def find_path_lengths(self) -> dict[tuple[int],int]:
+        '''
+        Find minimum path to all tiles.
 
-class RamField(Field):
-    def add_bytes(self, positions: list[Position]) -> None:
-        for p in positions:
-            self.set_value(p, '#')
+        Returns
+        -------
+        path_length: dict[tuple[int],int]
+          keys: positions on field, values: path lengths from start position away
 
-    def get_neighbors(self, position: Position) -> list[Position]:
-        y,x = position.y, position.x
-        y_list, x_list = [y-1,y,y+1,y], [x,x+1,x,x-1]
-        neighbors = [Position(i,j) for i,j in zip(y_list,x_list)]
-        validated = [n for n in neighbors if self.in_bounds(n)]
-        validated = [n for n in validated if self.get_value(n) != '#']
-        return validated
-
-    def find_path(self) -> None:
+        '''
         path_length = {(0,0): 0}
         tiles_to_check = {(0,0): self.get_neighbors(Position(0,0))}
-        # print(tiles_to_check)
         while len(tiles_to_check) > 0:
             previous = list(tiles_to_check.keys())[0]
             tile = tiles_to_check[previous].pop()
@@ -36,45 +25,41 @@ class RamField(Field):
                 path_length[tile.position()] = path_length[previous] + 1
                 tiles_to_check.update({tile.position(): self.get_neighbors(tile)})
 
-            # print(tiles_to_check)
             tiles_to_check = {k: v for k,v in tiles_to_check.items() if len(v) > 0}
-            # print(tiles_to_check)
 
         return path_length
 
-    def find_cutting_off_byte(self, positions: list[Position]) -> None:
+    def find_cutting_off_byte(self, positions: list[Position]) -> tuple[int]:
+        '''
+        Test if path to goal is free for all bytes step-by-step.
+
+        Parameters
+        ----------
+        positions: list[Position]
+          The bytes that fall down
+
+        Returns
+        -------
+        y,x: int,int
+          Tile position of cutting-off byte
+        '''
+        print('Testing bytes whether they cut off the path...')
         self.target = Position(self.Ny-1, self.Nx-1)
         # we know that the first 1024 are ok
         self.add_bytes(pos[:1024])
         for b in positions[1024:]:
-            print(b)
             self.add_bytes([b])
-            path_length = self.find_path()
-            print(path_length)
-            print(b)
-            print(path_length[self.target.position()])
+            path_length = self.find_path_lengths()
             if self.target.position() not in path_length:
-                print(b)
+                return b.position_tuple
 
 if __name__ == '__main__':
     input_filename = 'z-18-01-input.txt'
     # input_filename = 'z-18-02-actual-example.txt'
     empty_input = 'z-18-03-empty-field-large.txt'
     # empty_input = 'z-18-04-empty-field-small.txt'
-    pos = read_data(input_filename)
+    pos = day_18_01.read_data(input_filename)
     field = RamField(empty_input, datatype=str)
-    # print(field)
-    print(field.Nx, field.Ny)
-    # exit()
-    # print(field.data_list)
-    # field.add_bytes(pos[:1024])
 
-    # for p in pos[:12]:
-    #     print(field.data_list[p.y])
-    #     # field.data_list[p.y][p.x] = '#'
-    #     field.set_value(p, '#')
-
-    # print(field.find_path())
-    field.find_cutting_off_byte(pos)
-    # 139 is too low
-    # 140 is too low
+    y,x = field.find_cutting_off_byte(pos)
+    print(f'The byte cutting off the path is on {x},{y}.')

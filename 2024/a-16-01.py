d840466b315ba3df0c7be5c6a021c4edda034037
mpoeverlein <max.poeverlein@gmail.com>
2025-01-29 12:59:45 +0100

    clean up

--- 2024/a-16-01.py
+++ 2024/a-16-01.py
@@ -60,10 +60,7 @@ class Maze(Field):
             case '.':
                 self.free_fields.append(Position(y,x))
 
-    def test_trajectory(self) -> None:
-        self.trajectory.append(TrajectoryPoint(2,5,east))
-
-    def make_move_tree(self, move_depth=100) -> None:
+    def make_move_tree(self, move_depth: int=100) -> None:
         trajectory_string = ''
         current = Trajectory(self.start, '', 0)
         goal_trajectories = []
@@ -96,7 +93,6 @@ class Maze(Field):
             if i % 5 == 0:
                 trajectories = self.clean_trajectories(trajectories)
 
-        print('FINAL traj', goal_trajectories)
         self.trajectories = goal_trajectories
 
     def clean_trajectories(self, trajectories) -> list[Trajectory]:
@@ -111,13 +107,11 @@ class Maze(Field):
                 new_traj_dict[test_key] = traj
         return list(new_traj_dict.values())
 
-
     def find_finish_trajectories(self) -> None:
         trajectories = [t for t in self.trajectories if t.position.data() == self.end.data()]
         self.trajectories = trajectories
 
     def find_minimum_trajectory(self) -> Trajectory:
-        print(self.trajectories)
         return min(self.trajectories, key=lambda t: t.cost)
 
     def add_moves(self, current: Trajectory) -> list[Trajectory]:
@@ -146,13 +140,8 @@ class Maze(Field):
             case 'v': dx = 0; dy = 1
             case '<': dx = -1; dy = 0
         current_y, current_x = current_y+dy, current_x+dx
-
         current_pos = Position(current_y, current_x)
-        # print('start data, pos string, current pos', self.start.data(), current.trajectory_string, current_pos)
 
-        # if self.in_bounds(current_pos) and current_pos.position() not in self.barriers_tuples:
-        # if self.in_bounds(current_pos) and current_pos.one_d_pos in self.free_one_d:
-        # if self.in_bounds(current_pos) and current_pos.one_d_pos not in self.barriers_one_d:
         if self.in_bounds(current_pos) and self.is_free[current_pos.one_d_pos]:
             return (current_y, current_x)
         else:
@@ -169,70 +158,13 @@ class Maze(Field):
 
         return '\n'.join([''.join(i) for i in result])
 
-    def get_neighbors(self, position: Position) -> dict[str,Position]:
-        y,x = position.y, position.x
-        y_list, x_list = [y-1,y,y+1,y], [x,x+1,x,x-1]
-        neighbors = {direction: Position(i,j) for direction,i,j in zip(directions,y_list,x_list)}
-        validated = {n:v for n,v in neighbors.items() if self.in_bounds(v)}
-        validated = {n:v for n,v in validated.items() if v not in self.barriers}
-        return validated
-
-    @staticmethod
-    def get_rotation_cost(d1: str, d2: str, turn_cost: int=1000) -> int:
-        i1, i2 = directions.index(d1), directions.index(d2)
-        # print(d1, d2, turn_cost *  min(abs(i1-i2), abs((i1+2)%4 - (i2+2)%4)))
-        return turn_cost *  min(abs(i1-i2), abs((i1+2)%4 - (i2+2)%4))
-
-    def count_cost(self) -> dict[tuple[int], int]:
-        ''' cost to move forward: 1, cost to turn left/right: 1000 '''
-        cost_dictionary = {(self.start.y, self.start.x): 0}
-        number_of_checks_dictionary = {(self.start.y, self.start.x): 11}
-        start_tile = TrajectoryPoint(self.start.y, self.start.x, east)
-        tiles_to_check = [] # this list contains 2-ples with first item TrajectoryPoint() of initial and
-        # second item TrajectoryPoint() of where to go next
-        for direction, neighbor in self.get_neighbors(start_tile.position).items():
-            tiles_to_check.append((start_tile,
-                                   TrajectoryPoint(neighbor.y, neighbor.x, direction)))
-        while len(tiles_to_check) > 0:
-            print(tiles_to_check)
-            current, next_tile = tiles_to_check.pop()
-
-            next_cost = cost_dictionary[current.position_tuple] + 1
-            next_cost += self.get_rotation_cost(current.direction, next_tile.direction)
-            if next_tile.position_tuple not in cost_dictionary:
-                cost_dictionary[next_tile.position_tuple] = next_cost
-            else:
-                cost_dictionary[next_tile.position_tuple] = min(cost_dictionary[next_tile.position_tuple], next_cost)
-            if next_tile.position_tuple not in number_of_checks_dictionary:
-                number_of_checks_dictionary[next_tile.position_tuple] = 0
-
-            number_of_checks_dictionary[next_tile.position_tuple] += 1
-
-            for direction, neighbor in self.get_neighbors(next_tile.position).items():
-                if neighbor.position_tuple not in number_of_checks_dictionary:
-                    number_of_checks_dictionary[neighbor.position_tuple] = 0
-                if number_of_checks_dictionary[neighbor.position_tuple] > 10: continue
-                tiles_to_check.append((next_tile,
-                                   TrajectoryPoint(neighbor.y, neighbor.x, direction)))
-
-        return cost_dictionary
-
-
-
-
 
 if __name__ == '__main__':
     # maze = Maze('z-16-02-actual-example.txt')
     # maze = Maze('z-16-03-another-example.txt')
     maze = Maze('z-16-01-input.txt')
-    # maze.test_trajectory()
-    maze.make_move_tree(move_depth=700)
+    maze.make_move_tree(move_depth=550)
     maze.find_finish_trajectories()
-    print(maze.trajectories)
-    print(maze.find_minimum_trajectory())
-    exit()
-    print(maze)
-    cost_dict = maze.count_cost()
-    print(cost_dict)
-    print(cost_dict[(maze.end.y, maze.end.x)])
+    trajectory_cost = maze.find_minimum_trajectory().cost
+    print(f'The trajectory with the lowest cost has a cost of {trajectory_cost}.')
 

c2c8bac91fa4ac81b35d2b00f334fa3a39544ec3
mpoeverlein <max.poeverlein@gmail.com>
2025-02-01 18:59:16 +0100

    add docstrings for day 19

--- 2024/a-19-01.py
+++ 2024/a-19-01.py
@@ -1,41 +1,77 @@
 #!/usr/bin/env python3
 
 def read_data(input_filename: str) -> list[list[str]]:
+    '''
+    Read in data from input file and return as two lists: the building_blocks and the target sequences.
+
+    Parameters
+    ----------
+    input_filename: str
+
+    Returns
+    -------
+    building_blocks: list[str]
+      e.g. br,gb,ubw, etc.
+    targets: list[str]
+      e.g. bwrgbr,ubwbeg,etc.
+    '''
     with open(input_filename, 'r') as f:
         content = ''.join(f.readlines())
 
-    building_blocks, target = content.split('\n\n')
+    building_blocks, targets = content.split('\n\n')
     building_blocks = building_blocks.split(', ')
-    target = target.split('\n')
+    targets = targets.split('\n')
 
-    return building_blocks, target
+    return building_blocks, targets
 
-def build_from_blocks(building_blocks: list[str], target: str) -> bool:
+def can_be_built(building_blocks: list[str], target: str) -> bool:
+    '''
+    Check if target sequence can be built from building blocks.
+
+    Parameters
+    ----------
+    building_blocks: list[str]
+      e.g. br,gb,ubw, etc.
+    target: str
+      e.g. bwrgbr
+
+    Returns
+    -------
+    is_built: bool
+    '''
+
     test_sequences = {target}
     while len(test_sequences) > 0:
-        # print('test_sequence', test_sequences)
         test_sequence = test_sequences.pop()
-        # print('test_sequence', test_sequence)
         if test_sequence in building_blocks:
             return True
         for building_block in building_blocks:
             if test_sequence.startswith(building_block):
-                # print(building_block, test_sequence)
                 test_sequences.add(test_sequence[len(building_block):])
 
     return False
 
 def count_possible(building_blocks: list[str], targets: list[str]) -> int:
-    counter = 0
-    for target in targets:
-        if build_from_blocks(building_blocks, target):
-            counter += 1
-    return counter
+    '''
+    How many target sequences can be built?
+
+    Parameters
+    ----------
+    building_blocks: list[str]
+      e.g. br,gb,ubw, etc.
+    targets: list[str]
+      e.g. bwrgbr,ubwbeg,etc.
+
+    Returns
+    -------
+    n_buildable: int
+    '''
+
+    return len([target for target in targets if can_be_built(building_blocks, target)])
 
 if __name__ == '__main__':
     input_filename = 'z-19-01-input.txt'
     # input_filename = 'z-19-02-actual-example.txt'
     building_blocks, targets = read_data(input_filename)
-    print(building_blocks)
-    # print(build_from_blocks(building_blocks, 'brwrr'))
-    print(count_possible(building_blocks, targets))
+    n_possible = count_possible(building_blocks, targets)
+    print(f'{n_possible} designs are possible.')
--- 2024/a-19-02.py
+++ 2024/a-19-02.py
@@ -19,19 +19,35 @@ Example:
 import importlib
 day_19_01 = importlib.import_module('a-19-01')
 
-def print_possibilities(possibilities) -> None:
-    print(','.join(['_'.join(p) for p in possibilities]))
-
 def make_possibilities_tree(building_blocks: list[str], target: str) -> int:
+    '''
+    Given a target design and a list of building blocks, find the number of possible arrangements.
+
+    Parameters
+    ----------
+    building_blocks: list[str]
+      e.g. br,gb,ubw, etc.
+    targets: list[str]
+      e.g. bwrgbr,ubwbeg,etc.
+
+    Returns
+    -------
+    n_arrangements: int
+    '''
+    len_max = max([len(b) for b in building_blocks])
+    building_blocks_dict = {i: [] for i in range(1,len_max+1)}
+    for b in building_blocks:
+        building_blocks_dict[len(b)].append(b)
+
     if target == '': return 0
-    data_dict = reduce_string(building_blocks, target)
+    data_dict = reduce_string(building_blocks_dict, target, len_max)
     if len(data_dict) == 0: return 0
     while any(key != '' for key in data_dict):
         test_rest = list(data_dict.keys())[0]
         if test_rest == '': # don't pop the empty string!
             test_rest = list(data_dict.keys())[1]
         n_possibilities = data_dict.pop(test_rest)
-        test_dict = reduce_string(building_blocks, test_rest)
+        test_dict = reduce_string(building_blocks_dict, test_rest, len_max)
         if len(test_dict) == 0: continue # no possibilities!
         for k,v in test_dict.items():
             if k in data_dict:
@@ -42,44 +58,60 @@ def make_possibilities_tree(building_blocks: list[str], target: str) -> int:
     if len(data_dict) == 0: return 0
     return data_dict['']
 
-def reduce_string(building_blocks: list[str], target: str) -> None:
-    len_max = max([len(b) for b in building_blocks])
-    building_blocks_dict = {i: [] for i in range(1,len_max+1)} # TODO: move this to a smarter place (instead of re-computing it all the time)
-    for b in building_blocks:
-        building_blocks_dict[len(b)].append(b)
+def reduce_string(building_blocks_dict: dict[int,list[str]], target: str, len_max: int) -> dict[str,int]:
+    '''
+    Given an input string <target>, check which building blocks come next and return dictionary of remainders strings as keys and number of possible arrangement as values.
 
+    Parameters
+    ----------
+    building_blocks_dict: dict[int,list[str]]
+      keys: length of building blocks
+      values: all building blocks of certain length
+    target: str
+      e.g. gbbrwu
+    len_max: int
+      How long is the longest building block?
+
+    Returns
+    -------
+    data_dict: dict[str,int]
+      keys: remaining string
+      values: number of possible arrangements
+    '''
     data_dict = {}
     for i in range(1,min(len_max,len(target))+1):
         test_string = target[:i]
         rest_string = target[i:]
-        # print('LEN REST', rest_string, len(rest_string))
         for b in building_blocks_dict[i]:
             if b != test_string:
                 continue
-            print(b,test_string)
             if rest_string in data_dict:
                 data_dict[rest_string] += 1
             else:
                 data_dict[rest_string] = 1
-        print(data_dict)
     return data_dict
 
 def count_possibilities(building_blocks: list[str], targets: list[str]) -> int:
-    counter = 0
-    for target in targets:
-        print('TAR', target)
-        n_poss = make_possibilities_tree(building_blocks, target)
-        print(target,n_poss)
-        counter += n_poss
-        print('COUTNER', counter)
-    return counter
+    '''
+    The target designs can be built in how many ways?
+
+    Parameters
+    ----------
+    building_blocks: list[str]
+      e.g. br,gb,ubw, etc.
+    targets: list[str]
+      e.g. bwrgbr,ubwbeg,etc.
+
+    Returns
+    -------
+    n_possibilities: int
+    '''
+    return sum([make_possibilities_tree(building_blocks, target) for target in targets])
 
 if __name__ == '__main__':
     input_filename = 'z-19-01-input.txt'
     # input_filename = 'z-19-02-actual-example.txt'
     building_blocks, targets = day_19_01.read_data(input_filename)
-    print(building_blocks)
-    # make_possibilities_tree(building_blocks, targets[0])
     counter = count_possibilities(building_blocks, targets)
-    print(counter)
+    print(f'The number of possible arrangements is {counter}.')
 
